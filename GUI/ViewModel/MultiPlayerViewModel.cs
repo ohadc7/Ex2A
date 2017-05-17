eb5a434fcfc532c4c80b09a363d41614fd4aefff
ido <idoklein1@gmail.com>
2017-05-17 22:29:20 +0300

    not interesting changes

--- GUI/ViewModel/MultiPlayerViewModel.cs
+++ GUI/ViewModel/MultiPlayerViewModel.cs
@@ -13,5 +13,33 @@ namespace GUI.ViewModel
             this.model.Connect();
             //manage all the communication with the server
         }
+
+        public IClientModel Model
+        {
+            get { return model; }
+        }
+
+
+        private int mazeRowsDefinition;
+        public int MazeRowsDefinition
+        {
+            get { return mazeRowsDefinition; }
+            set { mazeRowsDefinition = value; }
+        }
+        private int mazeColsDefinition;
+        public int MazeColsDefinition
+        {
+            get { return mazeColsDefinition; }
+            set { mazeColsDefinition = value; }
+        }
+
+        public string mazeNameDefinition;
+
+        public string MazeNameDefinition
+        {
+            get { return mazeNameDefinition; }
+            set { mazeNameDefinition = value; ; }
+        }
+
     }
 }
